9c924119e780d4005ac57a0ff46563fa2d424bbf
Daniel Cousens <github@dcousens.com>
2017-03-28 16:15:03 +1100

    ranger: move Range to namespace

--- ranger.hpp
+++ ranger.hpp
@@ -32,74 +32,74 @@ namespace ranger {
 // 		r.front() = e;
 // 		r.popFront();
 // 	}
+
+	template <typename I>
+	struct Range {
+	public:
+		using value_type = typename std::remove_reference<decltype(*I())>::type;
+
+	private:
+		I _begin;
+		I _end;
+
+	public:
+		Range (I begin, I end) : _begin(begin), _end(end) {}
+
+		auto begin () const { return this->_begin; }
+		auto drop (size_t n) const { return ranger::drop(*this, n); }
+		auto empty () const { return this->_begin == this->_end; }
+		auto end () const { return this->_end; }
+		auto size () const {
+			const auto diff = std::distance(this->_begin, this->_end);
+			assert(diff >= 0);
+			return static_cast<size_t>(diff);
+		}
+		auto take (size_t n) const { return ranger::take(*this, n); }
+		auto& back () { return *(_end - 1); }
+		auto& front () { return *_begin; }
+		void popBack () { this->popBackN(1); }
+		void popBackN (size_t n) {
+			assert(n <= this->size());
+			this->_end -= n;
+		}
+		void popFront () { this->popFrontN(1); }
+		void popFrontN (size_t n) {
+			assert(n <= this->size());
+			this->_begin += n;
+		}
+
+		template <typename E>
+		void put (E e) { return ranger::put(*this, e); }
+
+		auto& operator[] (const size_t i) {
+			assert(i < this->size());
+			return *(this->_begin + i);
+		}
+
+		auto operator[] (const size_t i) const {
+			assert(i < this->size());
+			return *(this->_begin + i);
+		}
+
+		template <typename E>
+		auto operator< (const E& rhs) const {
+			return std::lexicographical_compare(this->begin(), this->end(), rhs.begin(), rhs.end());
+		}
+	};
 }
-
-template <typename I>
-struct Range {
-public:
-	using value_type = typename std::remove_reference<decltype(*I())>::type;
-
-private:
-	I _begin;
-	I _end;
-
-public:
-	Range (I begin, I end) : _begin(begin), _end(end) {}
-
-	auto begin () const { return this->_begin; }
-	auto drop (size_t n) const { return ranger::drop(*this, n); }
-	auto empty () const { return this->_begin == this->_end; }
-	auto end () const { return this->_end; }
-	auto size () const {
-		const auto diff = std::distance(this->_begin, this->_end);
-		assert(diff >= 0);
-		return static_cast<size_t>(diff);
-	}
-	auto take (size_t n) const { return ranger::take(*this, n); }
-	auto& back () { return *(_end - 1); }
-	auto& front () { return *_begin; }
-	void popBack () { this->popBackN(1); }
-	void popBackN (size_t n) {
-		assert(n <= this->size());
-		this->_end -= n;
-	}
-	void popFront () { this->popFrontN(1); }
-	void popFrontN (size_t n) {
-		assert(n <= this->size());
-		this->_begin += n;
-	}
-
-	template <typename E>
-	void put (E e) { return ranger::put(*this, e); }
-
-	auto& operator[] (const size_t i) {
-		assert(i < this->size());
-		return *(this->_begin + i);
-	}
-
-	auto operator[] (const size_t i) const {
-		assert(i < this->size());
-		return *(this->_begin + i);
-	}
-
-	template <typename E>
-	auto operator< (const E& rhs) const {
-		return std::lexicographical_compare(this->begin(), this->end(), rhs.begin(), rhs.end());
-	}
-};
 
 template <typename R>
 auto range (R& r) {
 	using iterator = decltype(r.begin());
 
-	return Range<iterator>(r.begin(), r.end());
+	return ranger::Range<iterator>(r.begin(), r.end());
 }
 
 template <typename R>
 auto retro (R& r) {
 	using reverse_iterator = std::reverse_iterator<decltype(r.begin())>;
 
-	return Range<reverse_iterator>(reverse_iterator(r.end()), reverse_iterator(r.begin()));
+	return ranger::Range<reverse_iterator>(reverse_iterator(r.end()), reverse_iterator(r.begin()));
 }
 
 // rvalue references wrappers
